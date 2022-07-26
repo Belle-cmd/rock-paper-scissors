dd5d622cf4227326945d070d90c7632e2c9c1260
BelleProjects <bautistadana55@gmail.com>
2022-07-26 00:37:18 -0600

    Add comments to empty functions

---
+++ play.js
@@ -0,0 +1,29 @@
+/*
+Function that randomly returns either ‘Rock’, ‘Paper’ or ‘Scissors’.
+This acts as the computer's play.
+Input:
+Output:
+*/
+function getComputerChoice() {
+
+}
+
+/*
+Function that plays a single round of Rock Paper Scissors.
+- parameters are case-sensitive; users can input "rock", "ROCK", "RocK" or any other variation
+Input:
+    playerSelection
+    computerSelection
+Output: 
+    string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+*/
+function playRound(playerSelection, computerSelection) {
+
+}
+
+/*
+Function that plays 5 rounds, keep score, and reports the winner or loser at the end
+*/
+function game() {
+    // playRound()
+}
